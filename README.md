894f08326d503dbf0bef81f001384b1e99902a5a
Alex Zorin <alex@zorin.id.au>
2018-12-05 09:50:20 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# certbot-cpanel-dns-auth-hook
+
+This is an "auth hook" for Certbot that enables you to perform DNS-01 authentication.
+
+It is suitable when you want to use Certbot to issue an e.g. wildcard certificate, but your domain's DNS is hosted in cPanel.
+
+All it requires is that you have cPanel login credentials, and that your cPanel account has the ability to set TXT records.
+
+## Usage
+
+These instructions assume you are on a shell as the `root` user.
+
+1. Download `cpanel-dns.py` somewhere onto your server. In this example, we will use `/etc/letsencrypt/cpanel-dns.py` as the location.
+2. `chmod 0700 /etc/letsencrypt-cpanel-dns.py && chown root:root /etc/letsencrypt-cpanel-dns.py`
+3. Modify the configuration section of `/etc/letsencrypt-cpanel-dns.py` :
+
+```python
+# Configure here
+# URL to your cPanel login
+CPANEL_URI = "https://cpanel.my-server.com:2083"
+# Normal cPanel login credentials
+CPANEL_AUTH = HTTPBasicAuth("username", "password")
+# If you don't have a trusted certificate on :2083
+CPANEL_INSECURE_VERIFY = False
+# Adjust based on the performance of your DNS cluster.
+CPANEL_BIND_DELAY = 15
+```
+
+4. Try issue a certificate now.
+
+```bash
+certbot certonly --manual \
+--manual-auth-hook "./cpanel-dns.py create" \
+--manual-cleanup-hook "./cpanel-dns.py delete" \
+-d "*.my.domain.example.com" -d "*.example.com"
+```
+5. If this succeeds, so should automatic renewal.
---
+++ cpanel-dns.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import requests
+from requests.auth import HTTPBasicAuth
+from urllib import urlencode
+import sys
+import os
+import string
+from time import sleep
+
+# Configure here
+# URL to your cPanel login
+CPANEL_URI = "https://cpanel.example.com:2083"
+# Normal cPanel login credentials
+CPANEL_AUTH = HTTPBasicAuth("username", "password")
+# If you don't have a trusted certificate on :2083
+CPANEL_INSECURE_VERIFY = False
+# Adjust based on the performance of your DNS cluster
+CPANEL_BIND_DELAY = 15
+
+
+def cpapi2_request(module, function, data=None):
+    params = {
+        "cpanel_jsonapi_user": CPANEL_AUTH.username,
+        "cpanel_jsonapi_apiversion": "2",
+        "cpanel_jsonapi_module": module,
+        "cpanel_jsonapi_func": function
+    }
+    url = "{}/json-api/cpanel?{}".format(CPANEL_URI, urlencode(params))
+
+    resp = requests.post(url, data=data, auth=CPANEL_AUTH)
+    resp.raise_for_status()
+
+    as_json = resp.json()
+    return as_json
+
+
+def normalize_fqdn(fqdn):
+    fqdn = string.lower(fqdn)
+    if fqdn[-1:] != '.':
+        fqdn = fqdn + '.'
+    return fqdn
+
+
+def find_zone_for_name(domain):
+    resp = cpapi2_request("ZoneEdit", "fetchzones")
+    zones = resp['cpanelresult']['data'][0]['zones']
+
+    # fetchzones doesn't have a trailing . on its zones
+    if domain[-1:] == '.':
+        domain = domain[:-1]
+
+    domain_split = domain.split('.')
+    found = None
+    while len(domain_split) > 0:
+        search = string.join(domain_split, ".")
+        if search in zones:
+            found = search
+            break
+        domain_split = domain_split[1:]
+
+    return found
+
+
+def list_records(zone):
+    resp = cpapi2_request("ZoneEdit", "fetchzone_records", {'domain': zone})
+    return resp['cpanelresult']['data']
+
+
+def create_record(domain, txt_value):
+    to_add = normalize_fqdn('_acme-challenge.{}'.format(domain))
+    print("Creating {} TXT: {}".format(to_add, txt_value))
+    zone = find_zone_for_name(domain)
+    create_params = {'domain': zone, 'name': to_add, 'type': 'TXT',
+                     'txtdata': txt_value}
+    cpapi2_request("ZoneEdit", "add_zone_record", create_params)
+
+    print("Will sleep {} seconds to wait for DNS cluster to reload".
+          format(CPANEL_BIND_DELAY))
+    sleep(CPANEL_BIND_DELAY)
+
+
+def remove_record(domain, txt_value):
+    to_remove = normalize_fqdn("_acme-challenge.{}".format(domain))
+    zone = find_zone_for_name(to_remove)
+    print "Removing {} TXT: {}".format(to_remove, txt_value)
+    recs = list_records(zone)
+
+    found = filter(
+        lambda rec:
+            'name' in rec and rec['name'] == to_remove and
+            'type' in rec and rec['type'] == 'TXT' and
+            rec['txtdata'] == txt_value,
+        recs)
+
+    if len(found) == 0:
+        print("Could not find record to remove: {}/{}".
+              format(to_remove, txt_value))
+        return
+
+    delete_params = {'domain': zone, 'line': found[0]['line']}
+    cpapi2_request("ZoneEdit", "remove_zone_record", delete_params)
+
+
+act = sys.argv[1]
+
+if act == "create":
+    create_record(os.environ["CERTBOT_DOMAIN"],
+                  os.environ["CERTBOT_VALIDATION"])
+elif act == "delete":
+    remove_record(os.environ["CERTBOT_DOMAIN"],
+                  os.environ["CERTBOT_VALIDATION"])
+else:
+    print("Unknown action: {}".format(act))
+    exit(1)
